1b52c0c9a094b66917deee7f1e6b97f44316e2c8
Plamena Petkova <pl.petkova@gmail.com>
2024-02-29 16:07:16 +0200

    Added form to add product and server action to write in db

---
+++ src/app/error.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function ErrorPage() {
+    <div className="">Something went wrong. Please refresh the page!</div>
+}
---
+++ src/components/FormSubmitButton.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ComponentProps } from "react";
+import { useFormStatus} from "react-dom"
+
+type FormSubmitButtonProps = {
+    children:React.ReactNode,
+    className?: string,
+} & ComponentProps<"button">
+
+export default function FormSubmitButton(
+    {children, className, ...props}:FormSubmitButtonProps
+) {
+
+    const { pending } = useFormStatus();
+
+    return (
+        
+        <button {...props} type="submit" disabled={pending} className={`btn btn-primary ${className}`}
+            
+        >
+            {pending === true && <span className="loading loading-spinner"/>}
+            {children}</button>
+    )
+    
+}
