9108862fc495f0cbf69add5c34c86a544a2c7e9e
Plamena Petkova <pl.petkova@gmail.com>
2024-03-05 13:10:19 +0200

    Added cart and dropdown menu and stated footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
     
       <body className={inter.className}>
+        <Navbar />
       <main className="p-4 max-w-7xl m-auto m-w-[300px]">
         {children}
         </main>
