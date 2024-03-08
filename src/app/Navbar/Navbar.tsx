7081f16911668c586ce1c281d2a955d615676c35
Plamena Petkova <pl.petkova@gmail.com>
2024-03-08 15:13:05 +0200

    Changes in authOptions to be able to build the app

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -5,7 +5,8 @@ import { getCart } from "@/lib/db/cart";
 import ShoppingCartButton from "./ShoppingCartButton";
 import UserMenuButton from "./UserMenuButton";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
+
 
 async function searchProducts(formData: FormData) {
   "use server";
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,7 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/db/prisma";
-
-import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
-import { env } from "process";
-import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
+import { authOptions } from "@/lib/authOptions";
 
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma) as Adapter,
-    providers: [
-        GoogleProvider({
-            clientId: env.GOOGLE_CLIENT_ID!,
-            clientSecret: env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    callbacks: {
-        session({session, user}) {
-            session.user.id = user.id
-            return session;
-        }
-    },
-    events: {
-        async signIn({user}) {
-            await mergeAnonymousCartIntoUserCart(user.id);
 
-        }
-    }
-}
-
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as never;
 
 export {handler as GET, handler as POST}
