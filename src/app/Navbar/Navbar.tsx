40e908b09a1bed684ce8fe73c766f803ff12f2df
Plamena Petkova <pl.petkova@gmail.com>
2024-03-22 21:38:17 +0200

    Navbar remove not used Image tag

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import { redirect } from "next/navigation";
-import Image from "next/image";
 import Link from "next/link";
 import { getCart } from "@/lib/db/cart";
 import { getServerSession } from "next-auth";
