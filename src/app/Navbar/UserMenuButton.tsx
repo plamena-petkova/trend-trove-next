aab64ba3c7799502ea5bdba8ac7503206b6a39c9
Plamena Petkova <pl.petkova@gmail.com>
2024-03-25 14:45:40 +0200

    Bug fix links user menu

--- src/app/Navbar/UserMenuButton.tsx
+++ src/app/Navbar/UserMenuButton.tsx
@@ -44,20 +44,8 @@ export default function UserMenuButton({ session }: UserMenuButtonProps) {
         tabIndex={0}
         className="dropdown-content menu rounded-box menu-sm z-30 mt-3 w-52 bg-base-100 p-2 shadow"
       >
-        <li>
-          {!user && (
-            <button>
-              <Link href={"/register"}>Register</Link>
-            </button>
-          )}
-        </li>
-        <li>
-          {user && (
-            <button>
-              <Link href={"/add-product"}>Add products</Link>
-            </button>
-          )}
-        </li>
+        <li>{!user && <Link href={"/register"}>Register</Link>}</li>
+        <li>{user && <Link href={"/add-product"}>Add products</Link>}</li>
         <li>
           {user ? (
             <button onClick={() => signOut({ callbackUrl: "/" })}>
